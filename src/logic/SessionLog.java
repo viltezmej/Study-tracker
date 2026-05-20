f321910cf5a56c94f0e552a86f1a1397e4c2232b
S0K1S <kristina.cesaityte@gmail.com>
2026-05-20 19:31:43 +0300

    Added a SessionLog file to store info about one finished session

---
+++ src/logic/SessionLog.java
@@ -0,0 +1,60 @@
+package logic;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Stores information about one finished study session.
+ * Part of the data/logic layer.
+ */
+
+public class SessionLog {
+    private LocalDateTime date;
+    private String subjectName;
+    private int studiedSeconds;
+    private int expEarned;
+
+    public SessionLog(String subjectName, int studiedSeconds, int expEarned){
+        this.date = LocalDateTime.now();
+        this.subjectName = subjectName;
+        this.studiedSeconds = studiedSeconds;
+        this.expEarned = expEarned;
+    }
+
+    public String getFormattedDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return date.format(formatter);
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public int getStudiedSeconds() {
+        return studiedSeconds;
+    }
+
+    public void setStudiedSeconds(int studiedSeconds) {
+        this.studiedSeconds = studiedSeconds;
+    }
+
+    public int getExpEarned() {
+        return expEarned;
+    }
+
+    public void setExpEarned(int expEarned) {
+        this.expEarned = expEarned;
+    }
+
+    public String getFormattedStudyTime(){
+        int hours = studiedSeconds / 3600;
+        int minutes = (studiedSeconds %3600) / 60;
+        int seconds = studiedSeconds % 60;
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+}
