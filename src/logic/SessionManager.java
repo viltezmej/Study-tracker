d9cca095832588c619d215adbfa40c04dfd54ce1
S0K1S <kristina.cesaityte@gmail.com>
2026-05-20 18:31:21 +0300

    Finished setting up the basic SessionManager class. Fixed some faults from the previous version. Will need to further update it to add the penalty

--- src/logic/SessionManager.java
+++ src/logic/SessionManager.java
@@ -26,8 +26,87 @@ public class SessionManager {
         targetTime = mins;
         remainingTime = mins*60; //in seconds
         studiedTime = 0; //in seconds
-        running = false;
+        running = true;
         onBreak = false;
         finished = false;
+    }
+
+    //called once every second by the GUI timer
+    public void tick(){
+        if (!running || onBreak || finished){
+            return;
+        }
+
+        if (remainingTime > 0){
+            remainingTime--;
+            studiedTime++;
+        }
+
+        if(remainingTime <= 0){
+            remainingTime = 0;
+            running = false;
+            finished = true;
+        }
+    }
+
+    public void toggleBreak(){
+        if (!finished) {
+            onBreak = !onBreak;
+        }
+    }
+
+    public void endSessionEarly(){
+        running = false;
+        finished = true;
+
+        //TODO: trigger EXP penalty in StatsCalculator
+    }
+
+    public int getTargetTime() {
+        return targetTime;
+    }
+
+    public void setTargetTime(int targetTime) {
+        this.targetTime = targetTime;
+    }
+
+    public int getRemainingTime() {
+        return remainingTime;
+    }
+
+    public void setRemainingTime(int remainingTime) {
+        this.remainingTime = remainingTime;
+    }
+
+    public int getStudiedTime() {
+        return studiedTime;
+    }
+
+    public void setStudiedTime(int studiedTime) {
+        this.studiedTime = studiedTime;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public boolean isOnBreak() {
+        return onBreak;
+    }
+
+    public void setOnBreak(boolean onBreak) {
+        this.onBreak = onBreak;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
     }
 }
