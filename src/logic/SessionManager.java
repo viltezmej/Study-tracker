9c6712b2ad3e39b39bffe36e7cbd5211e31c563c
S0K1S <kristina.cesaityte@gmail.com>
2026-05-20 18:22:31 +0300

    Added two new logic classes. Added a constructor to the SessionManager class which will be responsible for the countdown logic.

--- src/logic/SessionManager.java
+++ src/logic/SessionManager.java
@@ -1,4 +1,33 @@
 package logic;
-
+/**
+ * Handles the timer state for one study session.
+ */
 public class SessionManager {
+    private int targetTime;
+    private int remainingTime; //in seconds
+    private int studiedTime; //in seconds
+    private boolean running;
+    private boolean onBreak;
+    private boolean finished;
+
+
+    public SessionManager(){
+        targetTime = 0;
+        remainingTime = 0;
+        studiedTime = 0;
+        running = false;
+        onBreak = false;
+        finished = false;
+    }
+
+    //start a new study session
+
+    public void startSession(int mins){
+        targetTime = mins;
+        remainingTime = mins*60; //in seconds
+        studiedTime = 0; //in seconds
+        running = false;
+        onBreak = false;
+        finished = false;
+    }
 }
